15631a954706dec6af91e3b46eb65a3251fb2734
zh dk <zhdk@zhdeMacBook-Pro.local>
2017-08-21 14:04:52 +0800

    Initial Commit

---
+++ 进度条和滑动条的使用/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  进度条和滑动条的使用
+//
+//  Created by zh dk on 2017/8/21.
+//  Copyright © 2017年 zh dk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
